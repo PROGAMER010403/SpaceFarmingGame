89b8a5714e03a24202a6cbe61b32cfe7888d3383
GottaBeRyan <rayyanthakur@hotmail.com>
2023-07-13 06:21:00 +0500

    Fixed Branch & Cleanup
    Cleaned up the code and other than that, the test scene was breaking since apparently android scene isn't included in the files but was being used in the test scene. Fixed that.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -4,11 +4,11 @@ extends CharacterBody2D
 func bullet():
 	pass
 
+
 func _physics_process(delta):
 	velocity.x = 0
 	move_and_slide()
-	
+
 
 func damage_value():
 	return (20)
-
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 @export var speed = 200
 @export var health = 100
 @export var damage_to_player = 20
+
 var takes_damage = false
 var trapped = false
 var is_attacked = false
@@ -17,17 +17,13 @@ var speed_s = speed
 func _ready():
 	add_to_group("enemies")
 	anim_player()
-	
-	
+
+
 func _physics_process(delta):
-	
-	
 	velocity.x = -speed
 	_update_health()
 	move_and_slide()
 	laser_damage()
-	
-	
 
 
 func _update_health():
@@ -37,26 +33,29 @@ func _update_health():
 		healthbar.visible = false
 	else:
 		healthbar.visible = true
-		
+
+
 func laser_damage():
 	if lasered:
 		health -= 1
 		$laser_timer.start()
 		slower()
 
+
 func trap_damage(trap_dmg):
 	if trapped:
 		health -= trap_dmg
 		slower()
 		$trap_timer.start()
-	
+
 
 func damage(dmg):
 		health -= dmg
 		print("dmg taken")
 		slower()
 		$Timer.start()
-		
+
+
 func slower():
 	if lasered:
 		speed = speed * .2
@@ -64,10 +63,11 @@ func slower():
 		speed = 0
 	else:
 		speed = speed * .8
-	
-	
+
+
 func enemy():
 	pass
+
 
 func dmg_to_player():
 	return(damage_to_player)
@@ -76,24 +76,19 @@ func dmg_to_player():
 func _on_bullet_detector_body_entered(body):
 	if body.has_method("bullet"):
 		var damageAmount = body.damage_value()
-		damage(damageAmount)		
+		damage(damageAmount)
 		print("b")
-	pass 
 
 
 func _on_timer_timeout():
-	speed = speed_s
-	pass 
+	speed = speed_s 
 
 
 func _on_player_detector_body_entered(body):
 	if body.has_method("android"):
-		is_attacking = true	
+		is_attacking = true
 		speed = 0
 		anim_player()
-		
-	pass 
-	
 
 
 func anim_player():
@@ -102,40 +97,24 @@ func anim_player():
 	else:
 		$AnimatedSprite2D.play("attack_anim")
 		animation_finished = false
-		
-	
-	
+
+
 func _on_player_detector_body_exited(body):
 	if body.has_method("android"):
 		is_attacking = false
 		speed = speed_s
 		anim_player()
-	pass 
-
-
-
-	
-
-
-
-
-
-
-
 
 
 func _on_animated_sprite_2d_animation_finished():
 	print("done")
 	animation_finished = true
 	anim_player()
-	pass 
 
 
 func _on_laser_timer_timeout():
 	speed = speed_s
-	pass 
 
 
 func _on_trap_timer_timeout():
 	speed = speed_s
-	pass 
